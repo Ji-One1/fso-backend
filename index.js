ac97e2b56226ddb96cf857629aff7d60a4c496c8
Jione <jioneban@gmail.com>
2023-02-26 01:28:26 -0500

    implement single entry endpoint

--- index.js
+++ index.js
@@ -28,12 +28,24 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    if (!persons.some(person => person.id == id)){
+        return response.status(404).end()
+    }
+
+    const person = persons.find(person => person.id == id)
+    response.json(person)
+})
+
+
 app.get("/info", (request, response) => {
     const arrayLength = persons.length
     const date = Date()
 
     response.send(
-        "<div>Phonebook has info for "+ arrayLength +" people</div>"
+        "<div>Phonebook has info for " + arrayLength + " people</div>"
         + "<div>" + date + "</div"
     )
 })
