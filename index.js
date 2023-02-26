1c3480a67a86a37b157305175244e0bc13c0dfcd
Jione <jioneban@gmail.com>
2023-02-26 02:21:01 -0500

    add post endpoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000000);
+}
 
 persons = [
     { 
@@ -49,10 +54,27 @@ app.get("/info", (request, response) => {
     )
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(persons)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-    console.log(persons);
     response.status(204).end()
 
 })
