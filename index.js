fdd377e22be05d2f6de40088645210e5f53b7194
Jione <jioneban@gmail.com>
2023-03-01 21:54:07 -0500

    implement mongoDB get request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require("express")
 const morgan = require('morgan')
+const Contact = require('./models/mongo')
 const app = express()
 
 app.use(express.json())
@@ -17,37 +19,14 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 
 
 
-
-
 const generateId = () => {
     return Math.floor(Math.random() * 1000000000);
 }
 
-persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+    Contact.find({}).then(result => response.json(result))
+
 })
 
 app.get('/api/persons/:id', (request, response) => {
